e1949c83ccf6d734a1d9742e17bc11567ec19ae6
Luann <luann0881@gmail.com>
2025-09-25 22:05:29 -0300

    Adiciona arquivos do projeto LetrasMsc

---
+++ LetrasMsc/app.py
@@ -0,0 +1,20 @@
+import requests 
+import streamlit as st
+def buscar_letra(banda, musica):
+    endpoint = f"https://api.lyrics.ovh/v1/{banda}/{musica}"
+    response = requests.get(endpoint)
+    letra= response.json()['lyrics'] if response.status_code == 200 else""
+    return letra
+
+st.image("https://i.imgur.com/SmktDIH.png")
+st.title("letras de musicas")
+banda = st.text_input("digite o nome da banda", key="banda")
+musica =  st.text_input("digite o nome da musica", key="musica")
+pesquisar = st.button("pesquisar", key="pesquisar")
+if pesquisar:
+    letra = buscar_letra(banda, musica)
+    if letra:
+        st.success("Letra encontrada")
+        st.write(letra)
+    else:
+        st.error("letra nao encontrada")
